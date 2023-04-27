c83ebc7ec19206a396936e51bdfdf5da388549ff
ElinaDubrovska <elinadubrovska1801@gmail.com>
2023-04-27 12:59:46 +0300

    add contacts.json,  localstarage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,32 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
+import initialContacts from './contacts.json'
 
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
     
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts  !== null) {
+      
+      this.setState({ contacts: JSON.parse(savedContacts ) });
+    } else {
+    
+      this.setState({ contacts: initialContacts});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = data => {
     const contact = {
